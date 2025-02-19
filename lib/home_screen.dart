e7b2df7fa0be451a4c1eff92af7b8c27af8e66d1
Aimad Moustajil <moustajil.dev@gmail.com>
2025-02-19 10:58:44 +0100

    adding some change on it

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -46,108 +46,173 @@ class _HomeScreenState extends State<HomeScreen> {
                   fontSize: 18,
                   fontWeight: FontWeight.bold),
             ),
-            // ListView(
-            //   scrollDirection: Axis.horizontal,
-            //   children: [],
-            // )
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+            SizedBox(
+              height: 150,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(right: 20),
+                    child: Column(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            borderRadius: BorderRadius.circular(500),
+                          ),
+                          padding: EdgeInsets.all(10),
+                          child: Icon(
+                            Icons.laptop,
+                            size: 40,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "Labtop",
+                          style: TextStyle(
+                              color: const Color.fromARGB(255, 117, 117, 117),
+                              fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(right: 20),
+                    child: Column(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            borderRadius: BorderRadius.circular(500),
+                          ),
+                          padding: EdgeInsets.all(10),
+                          child: Icon(
+                            Icons.phone,
+                            size: 40,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "Phone",
+                          style: TextStyle(
+                              color: const Color.fromARGB(255, 117, 117, 117),
+                              fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(right: 20),
+                    child: Column(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            borderRadius: BorderRadius.circular(500),
+                          ),
+                          padding: EdgeInsets.all(10),
+                          child: Icon(
+                            Icons.electric_bike,
+                            size: 40,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "Electric",
+                          style: TextStyle(
+                              color: const Color.fromARGB(255, 117, 117, 117),
+                              fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(right: 20),
+                    child: Column(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            borderRadius: BorderRadius.circular(500),
+                          ),
+                          padding: EdgeInsets.all(10),
+                          child: Icon(
+                            Icons.card_giftcard,
+                            size: 40,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "Gifts",
+                          style: TextStyle(
+                              color: const Color.fromARGB(255, 117, 117, 117),
+                              fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(right: 20),
+                    child: Column(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            borderRadius: BorderRadius.circular(500),
+                          ),
+                          padding: EdgeInsets.all(10),
+                          child: Icon(
+                            Icons.electric_car,
+                            size: 40,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "Cars",
+                          style: TextStyle(
+                              color: const Color.fromARGB(255, 117, 117, 117),
+                              fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Text(
+              "Best Selling",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold),
+            ),
+            GridView(
+              gridDelegate:
+                  SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
               children: [
-                Column(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
+                Card(
+                  child: Column(
+                    children: [
+                      Container(
                         color: Colors.grey[200],
-                        borderRadius: BorderRadius.circular(500),
+                        child: Image.asset("assets/images/i1.jpg"),
                       ),
-                      padding: EdgeInsets.all(10),
-                      child: Icon(
-                        Icons.laptop,
-                        size: 40,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      "Labtop",
-                      style: TextStyle(
-                          color: const Color.fromARGB(255, 117, 117, 117),
-                          fontWeight: FontWeight.bold),
-                    )
-                  ],
-                ),
-                Column(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
+                      Container(
                         color: Colors.grey[200],
-                        borderRadius: BorderRadius.circular(500),
-                      ),
-                      padding: EdgeInsets.all(10),
-                      child: Icon(
-                        Icons.phone,
-                        size: 40,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      "Phone",
-                      style: TextStyle(
-                          color: const Color.fromARGB(255, 117, 117, 117),
-                          fontWeight: FontWeight.bold),
-                    )
-                  ],
-                ),
-                Column(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Colors.grey[200],
-                        borderRadius: BorderRadius.circular(500),
-                      ),
-                      padding: EdgeInsets.all(10),
-                      child: Icon(
-                        Icons.electric_bike,
-                        size: 40,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      "Electric",
-                      style: TextStyle(
-                          color: const Color.fromARGB(255, 117, 117, 117),
-                          fontWeight: FontWeight.bold),
-                    )
-                  ],
-                ),
-                Column(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Colors.grey[200],
-                        borderRadius: BorderRadius.circular(500),
-                      ),
-                      padding: EdgeInsets.all(10),
-                      child: Icon(
-                        Icons.shop_2,
-                        size: 40,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      "Clothes",
-                      style: TextStyle(
-                          color: const Color.fromARGB(255, 117, 117, 117),
-                          fontWeight: FontWeight.bold),
-                    )
-                  ],
+                        child: Image.asset("assets/images/i1.jpg"),
+                      )
+                    ],
+                  ),
                 )
               ],
             )
